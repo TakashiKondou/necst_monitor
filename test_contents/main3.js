9e65a45c3cd55bb3e91d7e81aa6f539ee2b731fb
TakashiKondou <tkondo@a.phys.nagoya-u.ac.jp>
2018-02-23 06:42:47 +0000

    Initial Commit

---
+++ test_contents/main3.js
@@ -0,0 +1,17 @@
+var ros = new ROSLIB.Ros({url : "ws://" + location.hostname + ":9000"});
+
+ros.on("connection", function() {console.log("websocket: connected"); });
+ros.on("error", function(error) {console.log("websocket error; ", error); });
+ros.on("close", function() {console.log("websocket: closed");});
+
+var ls = new ROSLIB.Topic({
+    ros : ros,
+    name : "/read_status",
+    messageType : "necst/Read_status_msg"
+});
+
+ls.subscribe(function(message) {
+    for( e in message){
+	document.getElementById(e).innerHTML = message[e];
+    }
+});
